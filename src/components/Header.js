3ea985c82cad9b5681bce999eae06f2ffd4963eb
Michael McQuade <giraffesyo@gmail.com>
2018-03-09 14:53:03 -0600

    Batman
    This commit has no parents

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'styles/styles.css'
+
+class Header extends React.PureComponent {
+render() {
+  return <header></header>
+}
+}
+
+export { Header }
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'styles/styles.css'
+
+class Home extends React.PureComponent {
+  render() {
+    return (
+      
+        <h1 className="light-purple-text">Hello World</h1>
+      
+    )
+  }
+}
+
+export { Home }
