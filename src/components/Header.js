9db9da97d36be9cf80b42af6ffd1f0e149cb26d9
Michael McQuade <giraffesyo@gmail.com>
2018-03-10 12:42:33 -0600

    Sr-only link descriptions
    Added alternate links for sr-only, added noopener and noreferrer for security per https://mathiasbynens.github.io/rel-noopener/

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,22 +11,30 @@ class Header extends React.PureComponent {
               className="nav-link fa fa-github"
               href="https://github.com/giraffesyo"
               target="_blank"
-            />
+              rel="noopener noreferrer"
+            >
+              <div class="sr-only">Michael McQuade's Github</div>
+            </a>
           </li>
           <li class="nav-item">
             <a
               className="nav-link fa fa-twitter"
               href="https://twitter.com/giraffesyo"
               target="_blank"
-            />
+              rel="noopener noreferrer"
+            >
+              <div class="sr-only">Michael McQuade's Twitter</div>
+            </a>
           </li>
           <li class="nav-item">
             <a
               className="nav-link fa fa-linkedin"
-              href="#"
               href="https://www.linkedin.com/in/mcquademichael/"
               target="_blank"
-            />
+              rel="noopener noreferrer"
+            >
+              <div class="sr-only">Michael McQuade's Linkedin</div>
+            </a>
           </li>
         </ul>
         <div className="navbar-brand light-purple-text">Michael McQuade</div>
