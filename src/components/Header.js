ff0cbcb1af2ebe652eced0ecec5dc1473ea2e386
Michael McQuade <giraffesyo@gmail.com>
2018-03-10 12:32:44 -0600

    Add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,37 @@ import React from 'react'
 import 'styles/styles.css'
 
 class Header extends React.PureComponent {
-render() {
-  return <header></header>
-}
+  render() {
+    return (
+      <nav className="navbar bg-dark">
+        <ul className="nav">
+          <li class="nav-item">
+            <a
+              className="nav-link fa fa-github"
+              href="https://github.com/giraffesyo"
+              target="_blank"
+            />
+          </li>
+          <li class="nav-item">
+            <a
+              className="nav-link fa fa-twitter"
+              href="https://twitter.com/giraffesyo"
+              target="_blank"
+            />
+          </li>
+          <li class="nav-item">
+            <a
+              className="nav-link fa fa-linkedin"
+              href="#"
+              href="https://www.linkedin.com/in/mcquademichael/"
+              target="_blank"
+            />
+          </li>
+        </ul>
+        <div className="navbar-brand light-purple-text">Michael McQuade</div>
+      </nav>
+    )
+  }
 }
 
 export { Header }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import 'styles/styles.css'
+import { Header } from 'components/Header'
 
 class Home extends React.PureComponent {
   render() {
     return (
-      
+      <div>
+        <Header />
         <h1 className="light-purple-text">Hello World</h1>
-      
+      </div>
     )
   }
 }
