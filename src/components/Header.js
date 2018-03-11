22fdb5209a723f5708d03bf5e17614c3fc755241
Michael McQuade <giraffesyo@gmail.com>
2018-03-11 01:34:42 -0600

    Fix typo in class attributes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,34 +6,34 @@ class Header extends React.PureComponent {
     return (
       <nav className="navbar bg-dark">
         <ul className="nav">
-          <li class="nav-item">
+          <li className="nav-item">
             <a
               className="nav-link fa fa-github"
               href="https://github.com/giraffesyo"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <div class="sr-only">Michael McQuade's Github</div>
+              <div className="sr-only">Michael McQuade's Github</div>
             </a>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <a
               className="nav-link fa fa-twitter"
               href="https://twitter.com/giraffesyo"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <div class="sr-only">Michael McQuade's Twitter</div>
+              <div className="sr-only">Michael McQuade's Twitter</div>
             </a>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <a
               className="nav-link fa fa-linkedin"
               href="https://www.linkedin.com/in/mcquademichael/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <div class="sr-only">Michael McQuade's Linkedin</div>
+              <div className="sr-only">Michael McQuade's Linkedin</div>
             </a>
           </li>
         </ul>
