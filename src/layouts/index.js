415517de19490304471f32a25d8329b4ce81528b
Michael McQuade <giraffesyo@gmail.com>
2018-03-25 16:23:52 -0600

    Add font-awesome

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './index.css'
 import { Header } from '../components/Header'
-
 
 export default ({ children }) => (
   <div>
