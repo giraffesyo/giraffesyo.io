cc2724bad38d26a5306d3b9b798c764ccbbc888e
Michael McQuade <giraffesyo@gmail.com>
2018-03-25 16:14:22 -0600

    Gatsby new

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Michael McQuade',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+  ],
+}
