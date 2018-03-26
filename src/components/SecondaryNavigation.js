592ecc98207689dcc8427ac7a52a52b72157dd68
Michael McQuade <giraffesyo@gmail.com>
2018-03-25 19:07:56 -0600

    Pointer on hover nav

--- src/components/SecondaryNavigation.js
+++ src/components/SecondaryNavigation.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Nav, NavItem, NavLink } from 'reactstrap'
+import styles from './SecondaryNavigation.module.css'
 
 class SecondaryNavigation extends React.PureComponent {
   handleItemClick = (e, { name }) =>
@@ -10,13 +11,13 @@ class SecondaryNavigation extends React.PureComponent {
     return (
       <Nav justified>
         <NavItem  className="light-purple-text">
-          <NavLink name='About Me' onClick={this.props.callback} active={activeItem === 'About Me'}>About Me</NavLink>
+          <NavLink className={styles.button} name='About Me' onClick={this.props.callback} active={activeItem === 'About Me'}>About Me</NavLink>
         </NavItem>
         <NavItem onClick={this.props.callback} className="light-purple-text">
-          <NavLink name='Projects' active={activeItem === 'Projects'}>Projects</NavLink>
+          <NavLink className={styles.button} name='Projects' active={activeItem === 'Projects'}>Projects</NavLink>
         </NavItem>
         <NavItem className="light-purple-text">
-          <NavLink name='Photos' onClick={this.props.callback} active={activeItem === 'Photos'}>Photos</NavLink>
+          <NavLink className={styles.button} name='Photos' onClick={this.props.callback} active={activeItem === 'Photos'}>Photos</NavLink>
         </NavItem>
       </Nav>
     )
