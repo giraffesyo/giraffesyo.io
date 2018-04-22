a61a07a23977fa989539299b5e88de3ea5cc0c29
Michael McQuade <giraffesyo@gmail.com>
2018-04-22 13:58:56 -0500

    Update path for gatsby-source-filesystem
    The images fell outside the previously configured path.

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/blog/posts`,
+        path: `${__dirname}/src/blog`,
       },
     },
   ],
