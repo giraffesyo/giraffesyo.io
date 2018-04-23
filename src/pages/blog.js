c8b829b4087a1c9194185dc040b1d91074930bad
Michael McQuade <giraffesyo@gmail.com>
2018-04-22 22:58:42 -0500

    Add Read More link

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -24,17 +24,20 @@ export default ({ data }) => {
       </Row>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
+          <Row style={{clear: 'both'}}>
+            <h2>{node.frontmatter.title}</h2>
+          </Row>
+          <Row>
+            <h6 className="code-font green-text">//{node.frontmatter.date}</h6>
+          </Row>
+          <Row>
+            <p style={{ marginLeft: 0 }}>{node.excerpt}</p>
+          </Row>
           <Link
             to={node.frontmatter.path}
             style={{ textDecoration: `none`, color: `inherit` }}
           >
-            <Row>
-              <h2>{node.frontmatter.title}</h2>
-            </Row>
-            <Row>
-              <h6 className="green-text"> {node.frontmatter.date}</h6>
-            </Row>
-            <p style={{ marginLeft: 0 }}>{node.excerpt}</p>
+            <Row style={{float: 'right'}}className="orange-text">Read more...</Row>
           </Link>
         </div>
       ))}
@@ -57,4 +60,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
