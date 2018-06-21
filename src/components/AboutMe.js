740ac9a59712739cf05514b34b433a4083c01985
Michael McQuade <giraffesyo@gmail.com>
2018-06-21 09:42:01 -0400

    Expand and update intro paragraph

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -22,14 +22,18 @@ class AboutMe extends React.PureComponent {
           <Col>
             <p>I'm Michael McQuade.</p>
             <p>
-              I'm a Computer Science student at Oklahoma State University.
-              However, I'm currently studying abroad at Monterrey Institute of
-              Technology and Higher Education in Mexico City, Mexico.
+              I'm a Computer Science student at Oklahoma State University. I
+              spent the last year studying my major abroad in Mexico City,
+              Mexico. The experience helped me broaden my horizons as a denizen
+              of the world while learning Computer Science from the most
+              prestigious school in Mexico.
             </p>
             <p>
               My experiences have been varied, from customer service to managing
               a pizza place. Now, I'm finally doing what I love, learning to
-              code!
+              code!  I'm currently working as an Junior
+              Developer at Foundation Software in Strongsville, Ohio, and will
+              return to finish my senior year this August. I expect to graduate May 2019.
             </p>
           </Col>
         </Row>
