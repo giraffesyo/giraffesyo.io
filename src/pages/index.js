f6e5442f291b2155389619d3c8d58e211a653c2c
Michael McQuade <giraffesyo@gmail.com>
2018-09-16 04:00:44 -0500

    Change layout to component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { BlogExcerpts } from '../components/BlogExcerpts'
 import { SecondaryNavigation } from '../components/SecondaryNavigation'
 import { AboutMe } from '../components/AboutMe'
 import { Photos } from '../components/Photos'
+import Layout from '../components/Layout'
 import Helmet from 'react-helmet'
 
 import { Container, Row, Col } from 'reactstrap'
@@ -25,44 +26,54 @@ class Home extends React.PureComponent {
     const { data } = this.props
     const { activeItem } = this.state
     return (
-      <Container>
-        <Helmet title="giraffesyo.io - Michael McQuade's Personal Site and blog" meta={[{name: 'description', content: `Michael McQuade's personal site: Michael is a computer science student at Oklahoma State University and has a passion for web devleopment. `}]}>
-          <html lang="en" />
-        </Helmet>
-        <Row>
-          <Col xs={12}>
-            <SecondaryNavigation
-              callback={this.changeActiveItem}
-              activeItem={activeItem}
-            />
-          </Col>
-        </Row>
+      <Layout>
+        <Container>
+          <Helmet
+            title="giraffesyo.io - Michael McQuade's Personal Site and blog"
+            meta={[
+              {
+                name: 'description',
+                content: `Michael McQuade's personal site: Michael is a computer science student at Oklahoma State University and has a passion for web devleopment. `
+              }
+            ]}
+          >
+            <html lang="en" />
+          </Helmet>
+          <Row>
+            <Col xs={12}>
+              <SecondaryNavigation
+                callback={this.changeActiveItem}
+                activeItem={activeItem}
+              />
+            </Col>
+          </Row>
 
-        {activeItem === 'Projects' ? (
-          <Row>
-            <Col xs={12}>
-              <Projects />
-            </Col>
-          </Row>
-        ) : activeItem === 'About Me' ? (
-          <Row>
-            <Col xs={12} md={8}>
-              <AboutMe />
-            </Col>
-            <Col xs={12} md={4}>
-              <BlogExcerpts data={data.allMarkdownRemark.edges} />
-            </Col>
-          </Row>
-        ) : activeItem === 'Photos' ? (
-          <Row>
-            <Col xs={12}>
-              <Photos />
-            </Col>
-          </Row>
-        ) : (
-          <div>You broke it...</div>
-        )}
-      </Container>
+          {activeItem === 'Projects' ? (
+            <Row>
+              <Col xs={12}>
+                <Projects />
+              </Col>
+            </Row>
+          ) : activeItem === 'About Me' ? (
+            <Row>
+              <Col xs={12} md={8}>
+                <AboutMe />
+              </Col>
+              <Col xs={12} md={4}>
+                <BlogExcerpts data={data.allMarkdownRemark.edges} />
+              </Col>
+            </Row>
+          ) : activeItem === 'Photos' ? (
+            <Row>
+              <Col xs={12}>
+                <Photos />
+              </Col>
+            </Row>
+          ) : (
+            <div>You broke it...</div>
+          )}
+        </Container>
+      </Layout>
     )
   }
 }
