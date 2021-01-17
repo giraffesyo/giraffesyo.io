6989603306f5b9f7430ff70ed75b35ea15ddd65f
Michael McQuade <michael@giraffesyo.io>
2021-01-17 15:10:54 -0600

    Move experience up

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,14 +20,15 @@ const IndexPage = () => {
       {/* <SecondaryNavigation /> */}
       <div className='section-header mt-2'>{'<AboutMe />'}</div>
       <AboutMeSection />
-      <div className='mb-5'>
-        <div className='section-header'>{'<Education />'}</div>
-        <EducationSection />
-      </div>
       <div className='flex flex-row flex-wrap'>
         <div className='section-header'>{'<Experience />'}</div>
         <WorkExperienceSection />
       </div>
+      <div className='mb-5'>
+        <div className='section-header'>{'<Education />'}</div>
+        <EducationSection />
+      </div>
+
       <div className='flex flex-row flex-wrap w-full'>
         <div className='section-header'>{'<Certifications />'}</div>
         <CertificationsSection />
