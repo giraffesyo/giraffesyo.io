4bfb720215503d18f2d5ec2afa24dc78bc8484e0
Michael McQuade <michael@giraffesyo.io>
2021-01-18 17:41:31 -0600

    Reorder certs

--- components/CertificationsSection/index.tsx
+++ components/CertificationsSection/index.tsx
@@ -1,5 +1,3 @@
-import { Project } from '../project'
-
 interface ICertificationProps {
   name: string
   imgSrc: string
@@ -37,16 +35,16 @@ const CertificationsSection: React.FC = () => {
   return (
     <div className='w-full flex flex-col md:flex-row justify-center text-center items-center'>
       <Certification
-        href='https://www.youracclaim.com/badges/37a99ac7-8e80-4693-8c09-48c709d9c8f9/public_url'
-        imgSrc='/images/certifications/aws_cloud_practitioner_badge.png'
-        name='AWS Certified Cloud Practitioner'
-        awardDate='4/8/2020'
-      ></Certification>
-      <Certification
         href='https://www.youracclaim.com/badges/6068be6e-ec80-483b-9782-d54875c8485d/public_url'
         imgSrc='/images/certifications/aws_developer_associate_badge.png'
         name='AWS Certified Developer - Associate'
         awardDate='12/11/2020'
+      ></Certification>
+      <Certification
+        href='https://www.youracclaim.com/badges/37a99ac7-8e80-4693-8c09-48c709d9c8f9/public_url'
+        imgSrc='/images/certifications/aws_cloud_practitioner_badge.png'
+        name='AWS Certified Cloud Practitioner'
+        awardDate='4/8/2020'
       ></Certification>
     </div>
   )
