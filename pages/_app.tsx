7a3fc1f8b2bd629540e5dd60d096f375995c375b
Michael McQuade <michael@giraffesyo.io>
2021-02-08 14:20:28 -0600

    Basic mdx support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,18 @@ import 'tailwindcss/tailwind.css'
 import '../styles/tailwind.css'
 import 'typeface-raleway'
 import '../styles/index.css'
-import {ThemeProvider} from 'next-themes'
+import { ThemeProvider } from 'next-themes'
 import type { AppProps} from 'next/app'
+import { MDXProvider } from '@mdx-js/react'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return (<ThemeProvider attribute="class">
-    <Component {...pageProps} />
-    </ThemeProvider>)
+  return (
+    <ThemeProvider attribute='class'>
+      <MDXProvider>
+        <Component {...pageProps} />
+      </MDXProvider>
+    </ThemeProvider>
+  )
 }
 
 export default App
