a29e38a585dc460d46b57d2c6ebff48b087e8828
Michael McQuade <michael@giraffesyo.io>
2021-02-09 15:10:51 -0600

    Manually specify slug

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -17,10 +17,6 @@ async function getFiles() {
   return directory
 }
 
-// export const getPosts =  async () => {
-
-// }
-
 export interface IFrontMatter {
   title: string
   description: string
@@ -36,23 +32,17 @@ export interface IBlogPost {
 }
 
 export const getPosts = async (): Promise<IBlogPost[]> => {
-  // List Posts
-  // const postsDirectory = path.resolve(process.cwd(), 'data', 'blog')
   const postFiles = await getFiles()
-  // Prepare posts off file system
   const postsPromises = postFiles.map((fileName) => {
     return getPost(fileName)
   })
   const postPromises = await Promise.all(postsPromises)
-  return postPromises.sort(
-    (a, b) =>
-      Number(new Date(b.frontMatter.date)) -
-      Number(new Date(a.frontMatter.date))
+  return postPromises.sort((a, b) =>
+    a.frontMatter.date.localCompare(b.frontMatter.date)
   )
 }
 
 const getPost = async (fileName) => {
-  // const source = fs.readFileSync(path.join(blogPath, `${slug}.mdx`), 'utf8')
   const source = fs.readFileSync(fileName, 'utf8')
 
   const { data, content } = matter(source)
@@ -66,7 +56,7 @@ const getPost = async (fileName) => {
       rehypePlugins: [mdxprism],
     },
   })
-  const { title, description, date } = data
+  const { title, description, date, slug } = data
   if (!title) {
     throw new Error(`No title in frontmatter for ${fileName}  `)
   }
@@ -84,40 +74,9 @@ const getPost = async (fileName) => {
       date,
       wordCount: content.split(/\s+/gu).length,
       readingTime: readingTime(content),
-      slug: getSlugFromPath(fileName) || null,
+      slug,
     },
   }
 
   return post
 }
-
-const getSlugFromPath = (path: string) => {
-  const slug = path.split(blogPath)[1].replace('.mdx', '')
-  return slug
-}
-
-// export async function getAllFilesFrontMatter(type) {
-//   const files = await getFiles(type)
-//   return files.reduce((allPosts, postPath) => {
-//     const source = fs.readFileSync(path.join(postPath), 'utf8')
-//     const { data } = matter(source)
-//     return [
-//       {
-//         ...data,
-//         slug: getSlugFromPath(postPath, type),
-//       },
-//       ...allPosts,
-//     ]
-//   }, [])
-// }
-
-// import fs from 'fs'
-// import matter from 'gray-matter'
-// import path from 'path'
-
-// export type BlogPost = {
-//   slug: string
-//   title: string
-//   date: string
-//   content: string
-// }
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,93 +1,5 @@
-// import Layout from '../../components/Layout'
-// import Link from 'next/link'
-// import { getAllPosts } from '../../lib/blogapi'
-// import Head from 'next/head'
-
-// export async function getStaticProps() {
-//   const allPosts = getAllPosts([
-//     'title',
-//     'date',
-//     'slug',
-//     'author',
-//     'coverImage',
-//     'excerpt',
-//     'hidden',
-//   ])
-
-//   return {
-//     props: { allPosts },
-//   }
-// }
-
-// export const BlogPostsIndexPage = ({ allPosts }) => {
-//   return (
-//     <Layout>
-//       <Head>
-//         <title>Blog - giraffesyo.io</title>
-//         <meta
-//           key='description'
-//           name='description'
-//           content='Software engineering blog - Technical posts about programming by Michael McQuade'
-//         />
-//       </Head>
-
-//       <div className='flex flex-row flex-wrap w-full '>
-// <h1 className='section-header'>
-//   {'<'}
-//   blog
-//   {'>'}
-// </h1>
-//         <div className='flex flex-row flex-wrap justify-center'>
-//           {allPosts
-//             .filter((post) => !post.hidden)
-//             .map(({ slug, title, date, author, excerpt, timeToReadString }) => (
-//               <Post
-//                 timeToReadString={timeToReadString}
-//                 author={author}
-//                 date={date}
-//                 excerpt={excerpt}
-//                 slug={slug}
-//                 title={title}
-//               />
-//             ))}
-//         </div>
-//       </div>
-//     </Layout>
-//   )
-// }
-
-// export default BlogPostsIndexPage
-
-// import { Post } from '../../components/Blog/Post'
-// import Layout from '../../layout'
-// import { posts } from '@lib/mdx'
-
-// export default function IndexPage() {
-//   return (
-//     <Layout
-//       pageTitle='Blog - giraffesyo.io'
-//       description='Software engineering blog - Technical posts about programming by Michael McQuade'
-//     >
-//       <h1 className='section-header'>
-//         {'<'}
-//         blog
-//         {'>'}
-//       </h1>
-//       <div className='flex flex-col md:flex-row items-center justify-center w-full '>
-//         {posts.map((post) => (
-//           <Post key={post.link} post={post} />
-//         ))}
-//         {posts.map((post) => (
-//           <Post key={post.link} post={post} />
-//         ))}
-//       </div>
-//     </Layout>
-//   )
-// }
-
 import { useState } from 'react'
 
-// import Container from '@/components/Container'
 import BlogPost from '@components/blog/BlogPost'
 import { getPosts } from '@lib/mdx'
 import Layout from 'layout'
@@ -96,7 +8,6 @@ import { InferGetStaticPropsType } from 'next'
 
 export const getStaticProps = async () => {
   const posts = await getPosts()
-
   return { props: { posts } }
 }
 
