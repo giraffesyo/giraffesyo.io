52b5594581a8058b539b532d6aeed42f66acfc2d
Michael McQuade <michael@giraffesyo.io>
2021-02-09 14:59:15 -0600

    Add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,28 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
-
+import { GA_TRACKING_ID } from '@lib/gtag'
 class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
         <Head />
+
         <body className='dark:bg-gray-codedark dark:text-white'>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <script dangerouslySetInnerHTML={{ __html: '0' }} />
           <Main />
           <NextScript />
