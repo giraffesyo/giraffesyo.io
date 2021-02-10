dcd61dc9e89f53699672153225af29c0cb355db3
Michael McQuade <michael@giraffesyo.io>
2021-02-10 15:39:23 -0600

    Support multiple blog posts

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -11,6 +11,7 @@ const rootDir = process.cwd()
 const blogPath = rootDir + '/data/blog/'
 const blogGlob = blogPath + '**/*.mdx'
 
+// get a list of all the files in the blog directory
 async function getFiles() {
   const directory = await globby(blogGlob)
   return directory
@@ -23,26 +24,30 @@ export interface IFrontMatter {
   readingTime: ReturnType<typeof readingTime>
   wordCount: number
   slug: string
-  date: string
+  date: string // comes in as Date from YAML but can't be serialized as such
   tags?: string[]
   image?: string
+  published?: boolean
 }
 export interface IBlogPost {
   mdxSource: MdxRemote.Source
   frontMatter: IFrontMatter
 }
 
+// get all the posts from fs (uses getPost)
 export const getPosts = async (): Promise<IBlogPost[]> => {
   const postFiles = await getFiles()
   const postsPromises = postFiles.map((fileName) => {
     return getPost(fileName)
   })
   const postPromises = await Promise.all(postsPromises)
+
   return postPromises.sort((a, b) =>
-    a.frontMatter.date.localCompare(b.frontMatter.date)
+    new Date(a.frontMatter.date) < new Date(b.frontMatter.date) ? 1 : -1
   )
 }
 
+// get a single post from the filesystem and prepare it for use
 const getPost = async (fileName) => {
   const source = fs.readFileSync(fileName, 'utf8')
 
@@ -57,35 +62,43 @@ const getPost = async (fileName) => {
       rehypePlugins: [mdxprism],
     },
   })
-  const { title, description, date, slug, summary, tags, image } = data
-  if (!slug) {
+
+  // check for "required" frontmatter, so that we can crash the build if something is missing that shouldn't be missing
+  if (!data.slug) {
     throw new Error(`No slug in frontmatter for ${fileName}`)
   }
-  if (!title) {
+  if (!data.title) {
     throw new Error(`No title in frontmatter for ${fileName}  `)
   }
-  if (!description) {
+  if (!data.description) {
     throw new Error(`No description in frontmatter for ${fileName}  `)
   }
-  if (!date) {
+  if (!data.date) {
     throw new Error(`No date in frontmatter for ${fileName}`)
   }
-  if (!summary) {
+  if (!data.summary) {
     throw new Error(`No summary in frontmatter for ${fileName}`)
   }
-  const post = {
+
+  // all the frontmatter parsed from the file is in data
+  // in order to recognize new fronmatter, we need to add it here
+  const frontMatter: IFrontMatter = {
+    published: data.published,
+    image: data.image || null,
+    tags: data.tags,
+    title: data.title,
+    description: data.description,
+    summary: data.summary,
+    date: data.date.toJSON(), // serialize the date as string
+    wordCount: content.split(/\s+/gu).length,
+    readingTime: readingTime(content),
+    slug: data.slug,
+  }
+
+  // data exposed in this object will be available in the post and on the post listings
+  const post: IBlogPost = {
     mdxSource,
-    frontMatter: {
-      image,
-      tags,
-      title,
-      description,
-      summary,
-      date,
-      wordCount: content.split(/\s+/gu).length,
-      readingTime: readingTime(content),
-      slug,
-    },
+    frontMatter,
   }
 
   return post
