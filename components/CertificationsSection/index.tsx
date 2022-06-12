547a3b65e7831e2e857ae53ac759c5449092e2f4
Michael McQuade <michael@giraffesyo.io>
2022-06-12 17:45:27 -0500

    Add SAA badge

--- components/CertificationsSection/index.tsx
+++ components/CertificationsSection/index.tsx
@@ -35,6 +35,12 @@ const CertificationsSection: React.FC = () => {
   return (
     <div className='w-full flex flex-col md:flex-row justify-center text-center items-center'>
       <Certification
+        href='https://www.credly.com/badges/2d3f5010-6db0-49eb-8309-4390ed370952/public_url'
+        imgSrc='/images/certifications/aws_solutions_architect_badge.png'
+        name='AWS Certified Solutions Architect - Associate'
+        awardDate='06/11/2021'
+      ></Certification>
+      <Certification
         href='https://www.youracclaim.com/badges/6068be6e-ec80-483b-9782-d54875c8485d/public_url'
         imgSrc='/images/certifications/aws_developer_associate_badge.png'
         name='AWS Certified Developer - Associate'
